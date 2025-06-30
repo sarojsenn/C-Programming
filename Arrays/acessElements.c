7e5cf373d5cf00f60aafb32475f163bc235e9c49
sarojsenn <sarojsen4952@gmail.com>
2025-06-30 15:23:23 +0530

    Create acessElements.c

---
+++ Arrays/acessElements.c
@@ -0,0 +1,22 @@
+// WAP to create an array that
+// can store N integers and
+// display the contents of the
+// array
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements:");
+    for(int i = 0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Array contents are:");
+    for(int i = 0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+
+}
